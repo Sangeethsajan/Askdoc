36b846c333e63a6fa7e7e06da2edbe7e07e61883
Sangeeth Sajan Baby <sangeethsajan13@gmail.com>
2025-02-10 22:44:03 -0600

    Changed the backend url

--- Frontend/askdoc/src/services/apiservices.ts
+++ Frontend/askdoc/src/services/apiservices.ts
@@ -1,6 +1,6 @@
 import axios,{AxiosRequestConfig} from 'axios';
 
-const BASE_URL = 'http://localhost:8000'
+const BASE_URL = 'http://172.190.112.201:8000'
 const makeAPIRequest = async (url: string, method: string, data: FormData,config?: AxiosRequestConfig) => {
   try {
     const response = await axios({
